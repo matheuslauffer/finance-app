e86ae386026a4317be972de323e92840a030cc7e
Matheus <matheuslauffer1@gmail.com>
2026-05-07 22:47:59 -0300

    feat: create payment methods schema

---
+++ db/schema/payment-methods.ts
@@ -0,0 +1,39 @@
+import {
+  pgTable,
+  uuid,
+  text,
+  integer,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+import { users } from './users';
+
+import { paymentMethodTypeEnum } from './enums';
+
+export const paymentMethods = pgTable(
+  'payment_methods',
+  {
+    id: uuid('id')
+      .defaultRandom()
+      .primaryKey(),
+
+    userId: uuid('user_id')
+      .references(() => users.id)
+      .notNull(),
+
+    name: text('name')
+      .notNull(),
+
+    methodType: paymentMethodTypeEnum(
+      'method_type'
+    ).notNull(),
+
+    closingDay: integer('closing_day'),
+
+    dueDay: integer('due_day'),
+
+    createdAt: timestamp('created_at')
+      .defaultNow()
+      .notNull(),
+  }
+);

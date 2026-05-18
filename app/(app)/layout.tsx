34499b5a8a672e212632de25dc57a7502db9efbc
Matheus <matheuslauffer1@gmail.com>
2026-05-17 23:45:48 -0300

    refactor: extract sidebar component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 
+import {
+  Sidebar,
+} from '@/app/components/sidebar';
+
 export default function
 AppLayout({
   children,
@@ -17,79 +21,7 @@ AppLayout({
 
       {/* SIDEBAR */}
 
-      <aside className="
-        w-64
-        bg-white
-        border-r
-        p-6
-        flex
-        flex-col
-        border-zinc-200
-      ">
-
-        <h1 className="
-          text-2xl
-            font-bold
-            mb-10
-            text-zinc-900
-        ">
-          Finance App
-        </h1>
-
-        <nav className="
-          flex
-          flex-col
-          gap-2
-        ">
-
-          <Link
-            href="/dashboard"
-
-            className="
-              p-3
-                rounded-xl
-                hover:bg-zinc-50
-                transition
-                text-zinc-700
-                font-medium
-            "
-          >
-            Dashboard
-          </Link>
-
-          <Link
-            href="/transactions"
-
-            className="
-              p-3
-                rounded-xl
-                hover:bg-zinc-50
-                transition
-                text-zinc-700
-                font-medium
-            "
-          >
-            Transações
-          </Link>
-
-          <Link
-            href="/transactions/new"
-
-            className="
-              p-3
-                rounded-xl
-                hover:bg-zinc-50
-                transition
-                text-zinc-700
-                font-medium
-            "
-          >
-            Nova transação
-          </Link>
-
-        </nav>
-
-      </aside>
+      <Sidebar />
 
       {/* CONTENT */}
 
